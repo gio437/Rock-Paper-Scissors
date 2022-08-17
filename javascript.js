ba7beb7a9c7b7bf324efbb8e7d9a93b09a7741cb
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-08-17 17:36:23 -0400

    added logic function

--- javascript.js
+++ javascript.js
@@ -1,20 +1,52 @@
 // return the words "rock, paper, scissors"
 // run through 1 iteration first
 //test out this first cycle in the console before going on
-//TO START OFF >
+//    <TO START OFF >
 // create a data structure with the strings
 //grab these strings
 // write function to randomly grab one of these strings
 
-
+//part 2
+//function with (two parameters)
+//grab a random value from the computer
+// grab the value inputed by the user
+// input logic to that determines who wins
+// return the winner
+//input the function getComputerChoice in for computerSelection
 
 function getComputerChoice(){
-const arr1 = ["Rock", "Paper", "Scissors"];
+const arr1 = ["rock", "paper", "scissors"];
 
 const random = Math.floor(Math.random() * arr1.length);
 console.log(random, arr1[random]);
 }
 
-getComputerChoice();
-
-
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beats Rock"
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if(playerSelection == "rock" && computerSelection == "rock"){
+        return "Tie! Try Again"
+    }
+    else if(playerSelection == "paper" && computerSelection == "paper"){
+        return "Tie! Try again"
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock"
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beats Paper"
+    }
+    else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper"
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats scissors"
+    }
+    else if(playerSelection == "scissors" && computerSelection == "scissors"){
+        return "Tie! Try again!"
+    }
+}
